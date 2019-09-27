eb1c2fb36044bdfcb0d62e2f722c6977874dc63e
danielachavez <00028116@uca.edu.sv>
2019-09-27 17:18:44 -0600

    ejercicicio 1

--- Laboratorios/Laboratorio5/ejercicio1.js
+++ Laboratorios/Laboratorio5/ejercicio1.js
@@ -1,44 +1,44 @@
 //Realice una función que reciba un arreglo como parámetro y liste los tipos de datos de sus elementos y cuantos de cada tipo. 
-var contadorString = 0;
-var contadorNumber = 0;
-var contadorBoolean = 0;
-var contadorNull   = 0;
-var contadorSymbol  = 0;
-var contadorUndefined  = 0;
+var cString = 0;
+var cNumber = 0;
+var cBoolean = 0;
+var cNull   = 0;
+var cSymbol  = 0;
+var cUndefined  = 0;
 
 function name(arr){
     for (let i = 0; i < arr.length; i++) {
         if(typeof(arr[i]) == 'string'){
-            contadorString = (contadorString + 1);
+            cString = (cString + 1);
 
         }
         if(typeof arr[i] == 'number'){
-            contadorNumber = (contadorNumber + 1);
+            cNumber = (cNumber + 1);
             
         }
         if((typeof arr[i]) == 'boolean'){
-            contadorBoolean= (contadorBoolean + 1);
+            cBoolean= (cBoolean + 1);
 
         }
         if(typeof arr[i] == 'null'){
-            contadorNull = (contadorNull + 1);
+            cNull = (cNull + 1);
         
         }
         if(typeof(arr[i]) == 'symbol'){
-            contadorSymbol = (contadorSymbol + 1);
+            cSymbol = (cSymbol + 1);
     
         }
         if(typeof(arr[i]) == 'undefined'){
-            contadorUndefined  = (contadorUndefined + 1);
+            cUndefined  = (cUndefined + 1);
         
         }
     }
-    console.log("cantidad de String =" + contadorString);
-    console.log("/ncantidad de Number =" + contadorNumber);
-    console.log("/ncantidad de Boolean =" + contadorBoolean);
-    console.log("/ncantidad de Null =" + contadorNull);
-    console.log("/ncantidad de Symbol =" + contadorSymbol);
-    console.log("/ncantidad de Undefined =" + contadorUndefined);
+    console.log("cantidad de String =" + cString);
+    console.log("/ncantidad de Number =" + cNumber);
+    console.log("/ncantidad de Boolean =" + cBoolean);
+    console.log("/ncantidad de Null =" + cNull);
+    console.log("/ncantidad de Symbol =" + cSymbol);
+    console.log("/ncantidad de Undefined =" + cUndefined);
 
 }
 
